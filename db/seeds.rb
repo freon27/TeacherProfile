3cbcc531a02c90624013127810912293587fa856
Nick Peters <freon27@gmail.com>
2012-04-09 11:51:24 +0100

    fixed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,12 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-User.create(
+User.create!(
   :first_name             => 'Admin', 
   :last_name              => 'User', 
   :email                  => ENV['admin_email'], 
   :password               => ENV['admin_password'],
   :password_confirmation  => ENV['admin_password'],
-  :admin                  => true
+  :admin                  => true,
+  :accepted_terms         => true
 )
