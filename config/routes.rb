9e7061b83eff97701509822f78c77e11acd68281
Nick Peters <freon27@gmail.com>
2012-05-12 11:07:03 +0100

    added subscription page

--- config/routes.rb
+++ config/routes.rb
@@ -105,6 +105,7 @@ Teacherprofile::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'high_voltage/pages#show', :id => 'about'
+  root :to => 'high_voltage/pages#show', :id => 'subscribe'
   
   match '/sign_in',  :to => 'sessions#new'
   match '/sign_up'  => 'users#new', :as => 'sign_up'
